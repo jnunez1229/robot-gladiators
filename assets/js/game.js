02e728e1794651da211f0192588fb51e7f160e83
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2020-12-13 11:55:27 -0600

    added console.log to js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is ian alert! JS is running!");
+const playerName = prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10+110);
+
+console.log("Our robot's name is " + playerName);
+
+function fight(){
+    alert("The fight has begun!");
+}
+
+//fight();
+
